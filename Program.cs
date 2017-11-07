0717d68b1b9e2a2cef129ccd2dbcd081b74ae94c
pre-12-HyungonKim <frozenabe86@gmail.com>
2017-11-08 00:20:12 +0900

    next

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace NumberGuesser
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      GetAppInfo();
+      GreetUser();
+      
+      while (true) 
+      {
+        Random random = new Random();
+        int correctNumber = random.Next(1, 10);
+        int guess = 0;
+
+        Console.WriteLine("Guess a number between 1 and 10");
+
+        while (guess != correctNumber) 
+        {
+          string inputGuess = Console.ReadLine();
+          if (!int.TryParse(inputGuess, out guess)) 
+          {
+            PrintColorMessage(ConsoleColor.Red, "Please use an actual number");
+            continue;
+          }
+          guess = Int32.Parse(inputGuess);
+
+          if (guess != correctNumber)
+          {
+            PrintColorMessage(ConsoleColor.Red, "Wrong number, please try again");
+          }
+        }
+
+        PrintColorMessage(ConsoleColor.Yellow, "You are correct");
+
+        Console.WriteLine("Play again? [Y or N]");
+        string answer = Console.ReadLine().ToUpper();
+
+        switch (answer)
+        {
+          case "Y":
+            continue;
+          case "N":
+            return;
+          default:
+            return;
+        }
+      }
+    }
+    
+    static void GetAppInfo()
+    {
+      string appName = "Number Guesser";
+      string appVersion = "1.0.0";
+      string appAuthor = "Abe";
+
+      Console.ForegroundColor = ConsoleColor.Green;
+      Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+      Console.ResetColor();
+    }
+
+    static void GreetUser()
+    {
+      Console.WriteLine("What is your name?");
+      string inputName = Console.ReadLine();
+      Console.WriteLine("Hello {0}, let's play a game...", inputName);
+    }
+
+    static void PrintColorMessage(ConsoleColor color, string message)
+    {
+      Console.ForegroundColor = color;
+      Console.WriteLine(message);
+      Console.ResetColor();
+    }
+  }
+}
